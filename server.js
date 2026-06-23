1ba921239d8b7e4957b23386c81b79173f82192e
thegreenportfolio <gabriel.vasile.ca@gmail.com>
2026-06-23 15:37:15 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.get("/api/events", async (req, res) => {
   try {
